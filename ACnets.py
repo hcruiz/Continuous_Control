d34f7d8a2c9cbfad082a558ecab152d96458496c
HCR <hans.ruiz.euler@hotmail.com>
2019-07-28 17:31:51 +0200

    stochastic policy implemented and debugged. Version is working

--- ACnets.py
+++ ACnets.py
@@ -16,16 +16,25 @@ class Policy(nn.Module):
         self.fc2 = nn.Linear(hl1_size, hl2_size)
         self.fc_out = nn.Linear(hl2_size, action_size)
         self.tanh = nn.Tanh()
-        
+        self.std = nn.Parameter(torch.ones(1,action_size))
         print('Actor has input size', inputsize,' hidden layer 1 size: ', hl1_size, ' hidden layer 2 size: ', hl2_size,
         ' action_size',action_size)
         
-    def forward(self, x):
+    def forward(self, x, *args):
         
         #From 33-dim state -> 4 actions
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return self.tanh(self.fc_out(x)) # action defined in [-1,1]
+        mean_action = self.tanh(self.fc_out(x)) # action defined in [-1,1]
+        probability = torch.distributions.Normal(mean_action, self.std)
+        if len(args)==0:
+            action = probability.sample()
+        elif len(args)==1:
+            action = args[0]
+        else:
+            assert False, 'Too many arguments, please give only states (and actions if needed).'
+        log_p = torch.sum(probability.log_prob(action),dim=-1)
+        return log_p, action
 
 class Critic(nn.Module):
 
